ae3e89f7759126ee031b18c2af220bb2e41b4986
Victor Lima <victorgon2001@hotmail.com.br>
2022-09-22 10:46:38 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,30 @@
+public class D {
+
+    private boolean D1;
+    private char D2;
+
+    public boolean getD1() {
+        return this.D1;
+    }
+
+    public void setD1(boolean D1) {
+        this.D1 = D1;
+    }
+
+    public char getD2() {
+        return this.D2;
+    }
+
+    public void setD2(char D2) {
+        this.D2 = D2;
+    }
+
+    public void MD1() {
+        System.out.println("Dlasse D - Método MD1");
+    }
+
+    public void MD2() {
+        System.out.println("Dlasse D - Método MD2");
+    }
+
+}
