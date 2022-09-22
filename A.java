ebdc06287bbe013d2b74ddc71e60086450faba43
Victor Gonçalves Lima <80169111+Victorgonl@users.noreply.github.com>
2022-09-22 09:32:34 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,30 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return this.A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    public float getA2() {
+        return this.A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public void MA1() {
+        System.out.println("Classe A - Método MA1");
+    }
+
+    public void MA2() {
+        System.out.println("Classe A - Método MA2");
+    }
+
+}
---
+++ B.java
@@ -0,0 +1,30 @@
+public class B {
+
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return this.B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return this.B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    public void MB1() {
+        System.out.println("Classe B - Método MB1");
+    }
+
+    public void MB2() {
+        System.out.println("Classe B - Método MB2");
+    }
+
+}
