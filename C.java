43540791510f01a30497efeec4d19f5704d23ae6
Victor Lima <victorgon2001@hotmail.com.br>
2022-09-22 09:47:46 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,30 @@
+public class C {
+
+    private String C1;
+    private String C2;
+
+    public String getC1() {
+        return this.C1;
+    }
+
+    public void setC1(String C1) {
+        this.C1 = C1;
+    }
+
+    public String getC2() {
+        return this.C2;
+    }
+
+    public void setC2(String C2) {
+        this.C2 = C2;
+    }
+
+    public void MC1() {
+        System.out.println("Classe C - Método MC1");
+    }
+
+    public void MC2() {
+        System.out.println("Classe C - Método MC2");
+    }
+
+}
